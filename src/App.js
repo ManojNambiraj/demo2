86b3572ab9aee9fab399baca9acf419cfb960fc2
manojn1106 <manojdeveloper1997@gmail.com>
2024-09-19 19:27:00 +0530

    Class2 codes

--- src/App.js
+++ src/App.js
@@ -2,17 +2,29 @@ import "./App.css";
 import Button from "./Button";
 
 function App() {
+  const handleClick = (data) => {
+    alert(data);
+  };
+
   return (
     <div className="App">
-      <h1>Hello World</h1>
-      <Button itemName={"All"} btnColor={"red"} />
+
+      <Button handleClick={handleClick} btnName={"All"} value={50} />
+      <Button handleClick={handleClick} btnName={"Movies"} value={100} />
+      <Button handleClick={handleClick} btnName={"Cars"} value={2} />
+      <Button handleClick={handleClick} btnName={"Bikes"} value={1000} />
+      <Button handleClick={handleClick} btnName={"Classes"} value={666} />
+      
+
+      {/* <h1>Hello World</h1> */}
+      {/* <Button itemName={"All"} btnColor={"red"} />
       <Button itemName={"Movies"} btnColor={"yellow"} />
       <Button itemName={"cars"} btnColor={"blue"} />
       <Button itemName={"bikes"} btnColor={"brown"} />
       <Button itemName={"Drama"} btnColor={"orange"} />
       <Button itemName={"Mobile"} btnColor={"crimson"} />
       <Button itemName={"Cycle"} btnColor={"pink"} />
-      <Button itemName={"College"} btnColor={"lavendar"} />
+      <Button itemName={"College"} btnColor={"lavendar"} /> */}
     </div>
   );
 }
--- src/Button.js
+++ src/Button.js
@@ -1,13 +1,15 @@
 import React from "react";
-// import "./Button.css"
 
 function Button(props) {
-  // console.log("props--->", );
 
   return (
     <>
-      <button style={{ backgroundColor: props.btnColor }} className="demoBTN">
-        {props.itemName}
+      <button
+        onClick={() => {
+          props.handleClick(props.value);
+        }}
+      >
+        {props.btnName}
       </button>
     </>
   );
